df6d3a67c8ea73588a00b23bd94340de70ea5c82
duekdms <mmp7afy8@naver.com>
2023-05-06 16:17:37 +0900

    S3

--- src/main/java/com/mju/Board/config/S3Config.java
+++ src/main/java/com/mju/Board/config/S3Config.java
@@ -1,6 +1,7 @@
 package com.mju.Board.config;
 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
@@ -12,12 +13,18 @@ import org.springframework.context.annotation.Configuration;
 public class S3Config {
 
     @Bean
+    public AwsCredentialsProvider awsCredentialsProvider() {
+        return ProfileCredentialsProvider.builder()
+                .profileName("BoardService")  // 사용할 프로필 이름 지정
+                .build();
+    }
+    @Bean
     public S3Client s3Client() {
-        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create("", "");
+//        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create("", "");
 
         return S3Client.builder()
                 .region(Region.AP_NORTHEAST_2)
-                .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+                .credentialsProvider(awsCredentialsProvider())
                 .build();
     }
 
