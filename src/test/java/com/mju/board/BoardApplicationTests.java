e7b20283317a38958fcfd06cb197e5a2f3a802c3
sangminee <lsmlee99@gmail.com>
2023-05-22 19:14:20 +0900

    fix : build error fix

--- src/test/java/com/mju/board/BoardApplicationTests.java
+++ src/test/java/com/mju/board/BoardApplicationTests.java
@@ -1,34 +1,34 @@
-package com.mju.board;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
-class BoardApplicationTests {
-
-	@Autowired
-	private WebApplicationContext wac;
-	private MockMvc mockMvc;
-
-	@BeforeEach
-	public void setup() {
-		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-	}
-
-	@Test
-	public void testRegisterQnA() throws Exception {
-
-
-	}
-
-}
+//package com.mju.board;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+//import org.springframework.web.context.WebApplicationContext;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@AutoConfigureMockMvc
+//class BoardApplicationTests {
+//
+//	@Autowired
+//	private WebApplicationContext wac;
+//	private MockMvc mockMvc;
+//
+//	@BeforeEach
+//	public void setup() {
+//		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+//	}
+//
+//	@Test
+//	public void testRegisterQnA() throws Exception {
+//
+//
+//	}
+//
+//}
